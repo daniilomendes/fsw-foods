355994d60d4c42babd3ef3dc64b9b47ce946b6cf
Danilo Mendes <danilo.mirandha@gmail.com>
2024-05-25 12:05:37 -0300

    refactor: move link inside button and add link in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <div className="relative h-[30px] w-[100px]">
-        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
-      </div>
+      <Link href="/">
+        <div className="relative h-[30px] w-[100px]">
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            sizes="100%"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </Link>
 
       <Button
         size="icon"
