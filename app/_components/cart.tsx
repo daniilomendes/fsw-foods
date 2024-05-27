60aa9bb5a7a630ef912c4d6faebdbcb5d3b09069
Danilo Mendes <danilo.mirandha@gmail.com>
2024-05-27 19:02:44 -0300

    feat: add sucess alert after order submit

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -19,8 +19,15 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "./ui/alert-dialog";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const Cart = () => {
+interface CartProps {
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const Cart = ({ setIsOpen }: CartProps) => {
+  const router = useRouter();
   const { data } = useSession();
   const { products, subTotalPrice, totalPrice, totalDiscounts, clearCart } =
     useContext(CartContext);
@@ -64,6 +71,15 @@ const Cart = () => {
       });
 
       clearCart();
+      setIsOpen(false);
+
+      toast("Pedido finalizado com sucesso!", {
+        description: "Você pode acompanhá-lo na tela dos seus pedidos.",
+        action: {
+          label: "Meus Pedidos",
+          onClick: () => router.push("/my-orders"),
+        },
+      });
     } catch (error) {
       console.log(error);
     } finally {
--- app/products/[id]/components/product-details.tsx
+++ app/products/[id]/components/product-details.tsx
@@ -167,7 +167,7 @@ const ProductDetails = ({
           <SheetHeader>
             <SheetTitle className="text-left">Sacola</SheetTitle>
           </SheetHeader>
-          <Cart />
+          <Cart setIsOpen={setIsCartOpen} />
         </SheetContent>
       </Sheet>
 
