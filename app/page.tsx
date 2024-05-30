ca36347ce39519c1f253201dfe64e0ccf34cb9be
Danilo Mendes <danilo.mirandha@gmail.com>
2024-05-29 22:44:28 -0300

    style: responsiveness to the top banner of the page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
 import RestaurantList from "./_components/restaurant-list";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   const products = await db.product.findMany({
@@ -29,12 +30,37 @@ export default async function Home() {
   return (
     <>
       <Header />
-      <div className="px-5 pt-6">
-        <Search />
+
+      <div className="md:flex md:h-[500px] md:gap-8 md:bg-[#EA1D2C]">
+        <div className="md:flex md:flex-1 md:flex-col md:items-start md:gap-7 md:px-32 md:py-32">
+          <div className="md:flex md:flex-col md:gap-4">
+            <h1 className="max-sm:hidden md:text-5xl md:font-bold md:text-white">
+              Está com fome?
+            </h1>
+            <p className="max-sm:hidden md:text-lg md:text-white">
+              Com apenas alguns cliques, encontre refeições acessiveis perto de
+              você.
+            </p>
+          </div>
+          <div className="max-sm:px-5 max-sm:pt-6 md:w-full">
+            <Search />
+          </div>
+        </div>
+
+        <div className="md:relative md:right-20 md:top-[147px] md:flex-1">
+          <Image
+            src="/hero-banner.png"
+            alt="Pizza imagem"
+            fill
+            className="max-sm:hidden md:absolute md:left-1/2 md:top-1/2 md:mx-auto md:max-h-[371px] md:max-w-[371px] md:object-contain"
+          />
+        </div>
       </div>
+
       <div className="px-5 pt-5">
         <CategoryList />
       </div>
+
       <div className="px-5 pt-6">
         <PromoBanner
           src="/promo-banner-01.png"
